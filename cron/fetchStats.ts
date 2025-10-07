ec16022cb359da768ba627097c8c01e5176cf42d
jojog <jojog@example.com>
2025-10-06 18:58:12 -0700

    :)

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -20,7 +20,7 @@ const getPlayerConnectCodes = async (): Promise<string[]> => {
     "KB#795", "KJH#23", "ZEOD#609" , "FITZ#433","PENG#444", "TYPH#474",
      "SF#818","TSUK#668", "DD#0", "PREE#696", "PAL#0",  "SS#03", "MEDZ#841",
      "FFF#641","LEXR#881","STAV#838","RAUL#132","LAXN#455","KDOG#261", "ARRW#135",
-     "AERI#564", "CHUG#596", "MOT#510","GOOS#584", "CHIC#937", "" 
+     "AERI#564", "CHUG#596", "MOT#510","GOOS#584", "CHIC#937", "ARTH#977" 
     // "PLAYER3#789"
   ];
 };
